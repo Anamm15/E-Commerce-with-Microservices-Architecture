527c1d17befa8f0ae74de1220fd29a2197b83066
Choirul Anam <choirulanm.15@gmail.com>
2025-11-02 19:52:32 +0700

    init user services

---
+++ backend-microservices/user-services/migrations/user.seeder.go
@@ -0,0 +1,98 @@
+package migrations
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"user-services/models"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+func SeedUsers(db *gorm.DB) error {
+	var count int64
+
+	if count > 0 {
+		log.Println("ℹ️  Users table already seeded, skipping...")
+		return nil
+	}
+
+	hashPassword := func(pw string) string {
+		hash, _ := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
+		return string(hash)
+	}
+
+	users := []models.User{
+		{
+			FullName:    "Admin One",
+			Username:    "admin1",
+			Email:       "admin1@example.com",
+			AvatarUrl:   "https://i.pravatar.cc/150?img=1",
+			Password:    hashPassword("password123"),
+			Role:        "admin",
+			IsConfirmed: true,
+			MemberSince: time.Now(),
+		},
+		{
+			FullName:    "Admin Two",
+			Username:    "admin2",
+			Email:       "admin2@example.com",
+			AvatarUrl:   "https://i.pravatar.cc/150?img=2",
+			Password:    hashPassword("password123"),
+			Role:        "admin",
+			IsConfirmed: true,
+			MemberSince: time.Now(),
+		},
+		{
+			FullName:    "User One",
+			Username:    "user1",
+			Email:       "user1@example.com",
+			AvatarUrl:   "https://i.pravatar.cc/150?img=3",
+			Password:    hashPassword("password123"),
+			Role:        "user",
+			IsConfirmed: true,
+			MemberSince: time.Now(),
+		},
+		{
+			FullName:    "User Two",
+			Username:    "user2",
+			Email:       "user2@example.com",
+			AvatarUrl:   "https://i.pravatar.cc/150?img=4",
+			Password:    hashPassword("password123"),
+			Role:        "user",
+			IsConfirmed: false,
+			MemberSince: time.Now(),
+		},
+		{
+			FullName:    "User Three",
+			Username:    "user3",
+			Email:       "user3@example.com",
+			AvatarUrl:   "https://i.pravatar.cc/150?img=5",
+			Password:    hashPassword("password123"),
+			Role:        "user",
+			IsConfirmed: true,
+			MemberSince: time.Now(),
+		},
+	}
+
+	for _, data := range users {
+		var user models.User
+		err := db.Where(&models.User{Username: data.Username}).First(&user).Error
+		if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+			return err
+		}
+
+		isData := db.Find(&user, "username = ?", data.Username).RowsAffected
+		if isData == 0 {
+			if err := db.Create(&data).Error; err != nil {
+				return err
+			}
+		}
+	}
+
+	log.Println("ℹ️  Users table seeded successfully...")
+
+	return nil
+}
