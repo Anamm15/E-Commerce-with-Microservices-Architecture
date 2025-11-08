8be64d919ec761ab2ebcfdb5e2d9321b2d869b05
Choirul Anam <choirulanm.15@gmail.com>
2025-11-08 19:38:25 +0700

    implement gRPC to service

---
+++ backend-microservices/product-services/internal/constants/category_constant.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	CATEGORY_RETRIEVED_SUCCESSFULLY = "Category retrieved successfully."
+	CATEGORY_CREATED_SUCCESSFULLY   = "Category created successfully."
+	CATEGORY_UPDATED_SUCCESSFULLY   = "Category updated successfully."
+	CATEGORY_DELETED_SUCCESSFULLY   = "Category deleted successfully."
+	CATEGORY_NOT_FOUND              = "Category not found."
+	CATEGORY_CREATION_FAILED        = "Failed to create category."
+	CATEGORY_UPDATE_FAILED          = "Failed to update category."
+	CATEGORY_DELETION_FAILED        = "Failed to delete category."
+)
---
+++ backend-microservices/product-services/internal/services/review_service.go
@@ -0,0 +1,56 @@
+package services
+
+import (
+	"context"
+
+	"product-services/internal/dto"
+	"product-services/internal/models"
+	"product-services/internal/repositories"
+)
+
+type ReviewService interface {
+	GetAllReviews(ctx context.Context) ([]dto.ReviewResponseDTO, error)
+	GetReviewProductID(ctx context.Context, productID uint) ([]dto.ReviewResponseDTO, error)
+	CreateReview(ctx context.Context, reviewRequest dto.CreateReviewRequestDTO) (dto.ReviewResponseDTO, error)
+	UpdateReview(ctx context.Context, reviewID uint, reviewRequest dto.UpdateReviewRequestDTO) (dto.ReviewResponseDTO, error)
+	DeleteReview(ctx context.Context, reviewID uint) error
+}
+
+type reviewService struct {
+	reviewRepository repositories.ReviewRepository
+}
+
+func NewReviewService(reviewRepository repositories.ReviewRepository) ReviewService {
+	return &reviewService{reviewRepository: reviewRepository}
+}
+
+func (s *reviewService) GetAllReviews(ctx context.Context) ([]dto.ReviewResponseDTO, error) {
+	return s.reviewRepository.GetAllReviews(ctx)
+}
+
+func (s *reviewService) GetReviewProductID(ctx context.Context, productID uint) ([]dto.ReviewResponseDTO, error) {
+	return s.reviewRepository.GetReviewByProductId(ctx, productID)
+}
+
+func (s *reviewService) CreateReview(ctx context.Context, reviewRequest dto.CreateReviewRequestDTO) (dto.ReviewResponseDTO, error) {
+	review := models.Review{
+		ProductID: reviewRequest.ProductID,
+		Rating:    reviewRequest.Rating,
+		Comment:   reviewRequest.Comment,
+	}
+	return s.reviewRepository.CreateReview(ctx, &review)
+}
+
+func (s *reviewService) UpdateReview(ctx context.Context, reviewID uint, reviewRequest dto.UpdateReviewRequestDTO) (dto.ReviewResponseDTO, error) {
+	review := models.Review{
+		ID:        reviewID,
+		ProductID: reviewRequest.ProductID,
+		Rating:    reviewRequest.Rating,
+		Comment:   reviewRequest.Comment,
+	}
+	return s.reviewRepository.UpdateReview(ctx, &review)
+}
+
+func (s *reviewService) DeleteReview(ctx context.Context, reviewID uint) error {
+	return s.reviewRepository.DeleteReview(ctx, reviewID)
+}
